ef27d9c96fc96af2ece66065d7adffd857bdc049
Phil Helm <git@phelm.co.uk>
2022-10-02 01:05:35 +0100

    squash! 🛠

--- server/impl.ts
+++ server/impl.ts
@@ -20,30 +20,106 @@ import {
   IRevealRequest,
 } from "../api/types";
 
-type InternalState = UserState;
+type InternalState = GameState;
+
+const removeCard = (hand: Card[], card: Card) => {
+  const index = hand.findIndex((c) => c === card);
+  return hand.filter((_, i) => i !== index);
+}
+
+const nextTurn = (players: Player[], turn: UserId): UserId => {
+  const index = players.findIndex((p) => p.id === turn);
+  return players[(index + 1) % players.length].id;
+}
 
 export class Impl implements Methods<InternalState> {
   initialize(ctx: Context, request: IInitializeRequest): InternalState {
     return {
-      hand: [],
       players: [],
+      stage: GameStage.FIRST,
       turn: undefined,
-      piles: [],
-      points: 0,
-      gameStage: 0,
+      winner: undefined
     };
   }
   joinGame(state: InternalState, userId: UserId, ctx: Context, request: IJoinGameRequest): Response {
-    return Response.error("Not implemented");
+    // TODO: Check if already joined
+    const alreadyJoined = false // TODO:
+    if (alreadyJoined) {
+      return Response.error("Already joined")
+    }
+    state.players.push({
+      id: userId,
+      points: 0,
+      hand: [],
+      pile: {cards: []}
+    })
+
+    return Response.ok();
   }
   startGame(state: InternalState, userId: UserId, ctx: Context, request: IStartGameRequest): Response {
-    return Response.error("Not implemented");
+    if (state.players.length < 2) {
+      // return Response.error("Not enough players")
+    }
+    state.players.forEach(p => {
+      p.hand = [Card.SKULL, Card.FLOWER, Card.FLOWER, Card.FLOWER],
+      p.pile = {cards: []}
+
+    })
+    state.stage = GameStage.FIRST,
+    state.winner = undefined
+
+    if (!state.turn) {
+      const firstPlayerIndex = ctx.chance.integer({min: 0, max: state.players.length - 1})
+      state.turn = state.players[firstPlayerIndex].id
+    }
+    return Response.ok();
   }
   placeCard(state: InternalState, userId: UserId, ctx: Context, request: IPlaceCardRequest): Response {
-    return Response.error("Not implemented");
+    const player = state.players.find(p => p.id === userId)
+    if (!player) return Response.error("Player not found")
+
+    if (state.stage === GameStage.FIRST) {
+      if (player?.pile.cards.length > 0)  return Response.error("You already placed a card")
+
+      const hasCard = player?.hand.includes(request.card)
+      if (!hasCard) return Response.error("You don't have that card")
+
+      const user = state.players.find(p => p.id === userId)!
+      user.pile.cards.push(request.card)
+      user.hand = removeCard(user.hand, request.card)
+
+      const allPlaced = state.players.every(p => p.pile.cards.length > 0)
+      if (allPlaced) state.stage = GameStage.PLACING
+
+      return Response.ok();
+
+    } else if (state.stage === GameStage.PLACING) {
+      if (state.turn !== userId) return Response.error("Not your turn")
+
+      const hasCard = player?.hand.includes(request.card)
+      if (!hasCard) return Response.error("You don't have that card")
+      const user = state.players.find(p => p.id === userId)!
+      user.pile.cards.push(request.card)
+      user.hand = removeCard(user.hand, request.card)
+
+      state.turn = nextTurn(state.players, state.turn)
+
+      return Response.ok();
+
+    } else {
+      return Response.error('Not in placing stage')
+    }
+
+
+
   }
   bid(state: InternalState, userId: UserId, ctx: Context, request: IBidRequest): Response {
-    return Response.error("Not implemented");
+    if (state.stage !== GameStage.PLACING && state.stage !== GameStage.BIDDING) return Response.error("Not in bidding stage")
+
+
+
+
+    return Response.ok()
   }
   pass(state: InternalState, userId: UserId, ctx: Context, request: IPassRequest): Response {
     return Response.error("Not implemented");
@@ -52,6 +128,24 @@ export class Impl implements Methods<InternalState> {
     return Response.error("Not implemented");
   }
   getUserState(state: InternalState, userId: UserId): UserState {
-    return state;
+    const player = state.players.find(p => p.id === userId)
+    if (!player) {
+      return {
+        hand: [],
+        players: state.players,
+        turn: state.turn,
+        piles: state.players.map(p => p.pile),
+        points: 0,
+        gameStage: state.stage
+      }
+    }
+    return {
+      hand: player?.hand || [],
+      players: state.players,
+      turn: state.turn,
+      piles: state.players.map(p => p.pile),
+      points: player.points,
+      gameStage: state.stage
+    };
   }
 }
