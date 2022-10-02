7999714f8675efae87ccc698417d7446de00c6f0
Phil Helm <git@phelm.co.uk>
2022-10-02 16:34:49 +0100

    add web client

---
+++ client/web/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: { target: "esnext" },
+  define: {
+    "process.env": {
+      COORDINATOR_HOST: process.env.COORDINATOR_HOST,
+      MATCHMAKER_HOST: process.env.MATCHMAKER_HOST,
+    },
+  },
+  server: { host: "0.0.0.0" },
+  clearScreen: false,
+});
